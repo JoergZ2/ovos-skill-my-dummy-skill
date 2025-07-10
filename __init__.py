14d9e22dce2087e10b6c44c6185e52a7d4a522ea
JoergZ2 <joerg.zywek@gmx.de>
2025-07-10 15:43:08 +0200

    Testing date functions

---
+++ __init__.py
@@ -0,0 +1,46 @@
+from ovos_utils import classproperty
+from ovos_utils.log import LOG
+from ovos_utils.process_utils import RuntimeRequirements
+from ovos_workshop.decorators import intent_handler
+from ovos_workshop.skills import OVOSSkill
+from ovos_bus_client.session import SessionManager
+from ovos_date_parser import extract_datetime, nice_date
+import datetime as dt
+import json
+today = dt.today()
+
+class MyDummySkill(OVOSSkill):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # be aware that below is executed after `initialize`
+        self.override = True
+
+    @classproperty
+    def runtime_requirements(self):
+        return RuntimeRequirements(
+            internet_before_load=False,
+            network_before_load=False,
+            gui_before_load=False,
+            requires_internet=False,
+            requires_network=False,
+            requires_gui=False,
+            no_internet_fallback=True,
+            no_network_fallback=True,
+            no_gui_fallback=True,
+        )
+    
+    def initialize(self):
+        #from template
+        self.settings.merge(DEFAULT_SETTINGS, new_only=True)
+        self.settings_change_callback = self.on_settings_changed
+    
+    @intent_handler('dummy1.intent')
+    def handle_dummy_1(self, message):
+        """
+        Handle the first dummy intent.
+        """
+        day = message.data.get('date')
+        day = extract_datetime(day, lang=self.lang)
+        LOG.info("Date is: " + str(day))
+        self.speak_dialog("dummy.1.response")
+
